328404571b4e769f05ee75ccde81184135dcf060
cahito <cahito@gmail.com>
2022-11-19 12:34:12 -0300

    Feat - Requisito um cumprido.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    qtd_estudantes_online = 0
+    if type(target_time) != int:
+        return None
 
-    raise NotImplementedError
+    try:
+        for aluno in permanence_period:
+            if target_time in range(aluno[0], aluno[1] + 1):
+                qtd_estudantes_online += 1
+    
+        return qtd_estudantes_online
+    except Exception:
+        return None
