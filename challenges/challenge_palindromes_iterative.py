012f72de1a6c4611692d8bc1d5e92e109e9f7eb4
cahito <cahito@gmail.com>
2022-11-21 19:45:53 -0300

    Feat - Requisito seis cumprido.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,5 +1,13 @@
 def is_palindrome_iterative(word):
-    print(word)
-    drow = reversed(word)
-    print(drow)
-    return word == drow
+    array = list(word)
+    if len(array) == 0:
+        return False
+    n = len(array) - 1
+    x= 0
+    while x <= n:
+        if array[x] != array[n]:
+            return False
+        x += 1
+        n -= 1
+    
+    return True
