c3a63c1f700943cd866302e18967ff46a8d067a7
cahito <cahito@gmail.com>
2022-11-21 16:16:04 -0300

    Feat - Requisito quatro iniciado. Vou fazer os outros antes de prosseguir nele.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,24 @@
-def is_anagram(first_string, second_string):
-    result = first_string == reversed(second_string)
+def order_word(word: str):
+    n = len(word)
+    if n == 1:
+        return word
+    for ordered_chars in range(n - 1):
+        for idx in range(0, n - 1 - ordered_chars):
+            if word[idx] > word[idx +1]:
+                curr_idx = word[idx]
+                print("curr_index", curr_idx)
+                word.replace(word[idx], word[idx +1], 1)
+                word.replace(word[idx + 1], curr_idx, 1)
+    print(word)
+    return word
 
-    return (first_string, reversed(second_string, result))
+
+def is_anagram(first_string: str, second_string:str):
+    if first_string == "" or second_string == "":
+        return (first_string, second_string, False)
+    first_ordered = order_word(first_string.casefold())
+    print(first_ordered)
+    second_ordered = order_word(second_string.casefold())
+    print(second_ordered)
+
+    return (first_ordered, second_ordered, first_ordered == second_ordered)
