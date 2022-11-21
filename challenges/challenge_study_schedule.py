48f3096501abfa82834124278c3594e910cdf611
cahito <cahito@gmail.com>
2022-11-21 20:57:45 -0300

    Fix - resolvendo problema de complexidade no requisito um...

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,9 @@
 def study_schedule(permanence_period, target_time):
     qtd_estudantes_online = 0
-    if type(target_time) != int:
-        return None
 
     try:
+        if type(target_time) != int:
+            raise TypeError
         for aluno in permanence_period:
             if target_time in range(aluno[0], aluno[1] + 1):
                 qtd_estudantes_online += 1
