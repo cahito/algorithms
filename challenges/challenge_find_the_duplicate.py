156f332f8148e302bb4f5b08da8f3c420e92ac2a
cahito <cahito@gmail.com>
2022-11-21 20:16:53 -0300

    Feat - Requisito cinco cumprido. Consegui simplificar o algoritimo!!! :rocket: #VQV!!

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -6,17 +6,14 @@ def find_duplicate(nums: list):
         return False
 
     # ordered_nums = order_word(nums)
+    nums.sort()
+
     n = len(nums) - 1
     x = 0
-    while x <= n:
-        temp_array = nums[x:]
-        curr_first = temp_array[0]
-        if type(curr_first) != int or curr_first < 0:
+    while x < n:
+        if type(nums[x]) != int or nums[x] < 0:
             return False
-        idx_min = temp_array.index(min(temp_array))
-        nums[x] = temp_array[idx_min]
-        nums[idx_min + x] = curr_first
-        if nums[x] in nums[x + 1:]:
+        if nums[x] == nums[x + 1]:
             return nums[x]
         x += 1
 
