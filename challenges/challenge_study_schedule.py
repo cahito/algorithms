55295160a4940cea15da3183392df776e4418dac
cahito <cahito@gmail.com>
2022-11-21 21:23:53 -0300

    Fix - Consegui reduzir a complexidade da função para menos de 6. Agora vai!

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,18 @@
 def study_schedule(permanence_period, target_time):
+    if type(target_time) != int:
+        return None
     qtd_estudantes_online = 0
 
-    try:
-        if type(target_time) != int:
-            raise TypeError
-        for aluno in permanence_period:
-            if target_time in range(aluno[0], aluno[1] + 1):
-                qtd_estudantes_online += 1
+    for aluno in permanence_period:
+        if (
+            type(aluno[0]) != int
+            or type(aluno[1]) != int
+            or aluno[0] < 0
+            or aluno[1] > 24
+        ):
+            return None
 
-        return qtd_estudantes_online
-    except Exception:
-        return None
+        if target_time in range(aluno[0], aluno[1] + 1):
+            qtd_estudantes_online += 1
+
+    return qtd_estudantes_online
