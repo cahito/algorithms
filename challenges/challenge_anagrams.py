f5e89332211bd75a3ea1d72c30f1ab3535828a29
cahito <cahito@gmail.com>
2022-11-21 22:35:53 -0300

    Fix - Requisito um refeito com merge sort, para tentar passar no avaliador do GH.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,17 +1,49 @@
-def order_word(word: list):
-    n = len(word) - 1
-    if n == 0:
+def order_word(word: list, x=0, n=None):
+    if n is None:
+        n = len(word)
+    if n == 1:
         return word
-    x = 0
-    while x < n:
-        temp_array = word[x:]
+    if (n - x) > 1:
+        mid = (x + n) // 2
+        order_word(word, x, mid)
+        order_word(word, mid, n)
+        word_merge(word, x, mid, n)
+
+    """ while x < n:
+        temp_array = word[x:n]
         curr_first = temp_array[0]
+        curr_last = temp_array[-1]
         idx_min = temp_array.index(min(temp_array))
+        idx_max = temp_array.index(max(temp_array))
         word[x] = temp_array[idx_min]
+        word[n - 1] = temp_array[idx_max]
         word[idx_min + x] = curr_first
+        word[idx_max - x] = curr_last
         x += 1
+        n -= 1 """
 
     return word
+
+
+def word_merge(word, x, mid, n):
+    left = word[x:mid]
+    right = word[mid:n]
+
+    left_index, right_index = 0, 0
+
+    for idx in range(x, n):
+        if left_index >= len(left):
+            word[idx] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            word[idx] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            word[idx] = left[left_index]
+            left_index = left_index + 1
+        else:
+            word[idx] = right[right_index]
+            right_index = right_index + 1
 
 
 def is_anagram(first_string: str, second_string: str):
